4016a82921f184a9e064779820550165cf7f223c
THEDARKLORD <THEDARKLORD>
2023-02-15 14:49:33 +0530

    ESL LAB3

---
+++ SEM 4/ESL/LAB 3/Q2/l3q2.s
@@ -0,0 +1,22 @@
+		AREA RESET,DATA,READONLY
+		EXPORT __Vectors
+__Vectors
+		DCD 0x10001000
+		DCD Reset_Handler
+		ALIGN
+		AREA mycode,CODE,READONLY
+		ENTRY
+		EXPORT Reset_Handler
+Reset_Handler 
+		LDR R0,=N1
+		LDR R1,[R0]
+		LDR R6,=DST
+		MLA R2,R1,R1,R1
+		LSR R2,#1
+		STR R2,[R6]
+STOP	
+		B STOP
+N1 DCD 10
+		AREA mydata,DATA,READWRITE
+DST DCD 0
+		END
---
+++ SEM 4/ESL/LAB 3/Q4/l2q4.s
@@ -0,0 +1,27 @@
+		AREA RESET,DATA,READONLY
+		EXPORT __Vectors
+__Vectors
+		DCD 0x10001000
+		DCD Reset_Handler
+		ALIGN
+		AREA mycode,CODE,READONLY
+		ENTRY
+		EXPORT Reset_Handler
+Reset_Handler
+		LDR R0,=N
+		LDR R1,[R0]
+		LDR R6,=DST
+		AND R1,#0xF0
+		LSR R1,#4
+		ADD R1,#0x30
+		STRB R1,[R6],#1
+		LDR R1,[R0]
+		AND R1,#0x0F
+		ADD R1,#0x30
+		STRB R1,[R6]
+STOP	
+		B STOP
+N DCD 0x12
+		AREA mydata,DATA,READWRITE
+DST DCD 0
+		END
