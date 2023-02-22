bedd17759405606b551ed5ed6716a348588aa6ba
THEDARKLORD <THEDARKLORD>
2023-02-22 16:25:00 +0530

    ES LAB4

---
+++ SEM 4/ESL/LAB 4/Q1/l4q1.s
@@ -0,0 +1,30 @@
+		AREA RESET,DATA,READONLY
+		EXPORT __Vectors
+__Vectors
+		DCD 0x10001000
+		DCD Reset_Handler
+		ALIGN
+		AREA mycode,CODE,READONLY
+		ENTRY
+		EXPORT Reset_Handler
+Reset_Handler
+		LDR R0,=NUM
+		LDR R1,[R0]
+		LDR R0,=DST
+		MOV R3,#1
+		MOV R4,#0xA
+		MOV R5,#0
+		MOV R7,#0xF
+UP		MOV R6,R1
+		AND R6,R7
+		MLA R5,R6,R3,R5
+		MUL R3,R4
+		LSR R1,#4
+		CMP R1,#0
+		BNE UP
+		STR R5,[R0]
+STOP	B STOP
+NUM DCD 0x35
+		AREA mydata,DATA,READWRITE
+DST DCD 0
+		END
