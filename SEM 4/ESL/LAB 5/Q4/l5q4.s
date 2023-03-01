2e73bc23e920998f558555207433365b00e7574d
THEDARKLORD <THEDARKLORD>
2023-03-01 16:38:28 +0530

    ES LAB 5

---
+++ SEM 4/ESL/LAB 5/Q4/l5q4.s
@@ -0,0 +1,46 @@
+		AREA RESET,DATA,READONLY
+		EXPORT __Vectors
+__Vectors
+		DCD 0x10001000
+		DCD Reset_Handler
+		ALIGN
+		AREA mycode,CODE,READONLY
+		ENTRY
+		EXPORT Reset_Handler
+Reset_Handler
+		LDR R0,=10
+		LDR R1,=5
+		LDR R2,=4
+		LDR R3,=2
+		LDR R4,=6
+		LDR R5,=9
+		LDR R6,=8
+		LDR R7,=7
+		LDR R8,=1
+		LDR R9,=3
+		STM R13!,{R0-R9}
+		MOV R0,R13
+L2		MOV R1,R0
+		MOV R3,R0
+		SUB R3,#4
+		ADD R11,R10,#1
+L1		LDMDB R3,{R4}
+		LDMDB R1,{R7}
+		CMP R4,R7
+		BCC DOWN
+		MOV R1,R3
+DOWN	SUB R3,#4
+		ADD R11,#1
+		CMP R11,#10
+		BNE L1
+		LDMDB R0!,{R2}
+		LDMDB R1!,{R6}
+		STM R1,{R2}
+		STM R0,{R6}
+		ADD R10,#1
+		CMP R10,#9
+		BNE L2
+		LDMDB R13!,{R0-R9}
+STOP	B STOP
+		AREA mydata,DATA,READWRITE
+		END
