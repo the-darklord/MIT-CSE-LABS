914d3f557e160b9e665e40016015454d6f438762
THEDARKLORD <saimihirnath@protonmail.com>
2023-04-02 18:18:13 +0530

    DAA LAB 4

--- SEM 4/DAAL/LAB 4/q1.c
+++ SEM 4/DAAL/LAB 4/q1.c
@@ -1,76 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
-void store(int* minArr,int *ind,int n)
+void swap(int* a,int* b)
 {
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void evaluate(int** cm,int* in,int n,int* mincost,int* minarr)
+{
+    int sum=0;
     for(int i=0;i<n;i++)
     {
-        minArr[i]=ind[i]+1;
+        sum += cm[i][in[i]];
+    }
+    if(sum<*mincost)
+    {
+        memcpy(minarr,in,n*sizeof(int));
+        *mincost=sum;
     }
 }
 
-void swap( int *a, int *b) 
-{ 
-  int temp = *a; 
-  *a = *b; 
-  *b = temp; 
-} 
-
-void permute(int**jp,int *ind,int start,int end,int n,int *minArr,int *minCost) 
+void generate_permutations(int** cm,int* in,int n,int* mincost,int* minarr,int i)
 {
-    if(start==end)
+    if(i==n)
     {
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            sum+=jp[i][ind[i]];
-        }
-        if(*minCost==0||*minCost>sum)
-        {
-            *minCost=sum;
-            store(minArr,ind,n);
-        }
-        
+        evaluate(cm,in,n,mincost,minarr);
     }
     else
     {
-        for(int i=start;i<=end;i++)
+        for(int j=i;j<n;j++)
         {
-            swap((ind+start),(ind+i));
-            permute(jp,ind,start+1,end,n,minArr,minCost);
-            swap((ind+start),(ind+i));
+            swap(&in[i],&in[j]);
+            generate_permutations(cm,in,n,mincost,minarr,i+1);
+            swap(&in[i],&in[j]);
         }
     }
-} 
+}
 
 void main()
 {
     int n;
-    printf("Enter Number of Persons/Jobs : \t");
+    printf("Enter No. of Jobs/Persons : \t");
     scanf("%d",&n);
-    int **arr=(int **)calloc(n,sizeof(int *));
-    for(int i=0;i<n;i++)
-    {
-        arr[i]=(int *)malloc(n*sizeof(int));
-        for(int j=0;j<n;j++)
-        {
-            printf("Enter Assignment Cost of J%d , P%d : \t",i+1,j+1);
-            scanf("%d",&arr[i][j]);
-        }
-    }
-    int ind[n];
-    int minSal=0,minArr[n];
+    int** costmatrix;
+    costmatrix=(int**)calloc(n,sizeof(sizeof(int*)));
+    int* ind;
+    ind=(int*)malloc(n*sizeof(int));
+    printf("\nEnter Cost Matrix : \n");
     for(int i=0;i<n;i++)
     {
         ind[i]=i;
-        minArr[i]=0;
+        costmatrix[i]=(int*)malloc(n*sizeof(int));
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&costmatrix[i][j]);
+        }
     }
-    permute(arr,ind,0,n-1,n,minArr,&minSal);
-    printf("\nMinimum Cost is %d\n",minSal);
-    printf("For the Order\n");
+    int mincost=INT_MAX;
+    int minarr[n];
+    generate_permutations(costmatrix,ind,n,&mincost,minarr,0);
+    printf("\nMinimum Cost is %d\nFor the Order : \t",mincost);
     for(int i=0;i<n;i++)
     {
-        printf("%d ",minArr[i]);
+        printf("%d ",minarr[i]+1);
     }
     printf("\n");
 }
