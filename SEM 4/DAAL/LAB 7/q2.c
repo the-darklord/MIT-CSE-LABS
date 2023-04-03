be4ee0e469c534ab15d0ecb8487809da934575e4
THEDARKLORD <saimihirnath@protonmail.com>
2023-04-03 17:55:10 +0530

    DAA LAB 7

--- SEM 4/DAAL/LAB 7/q2.c
+++ SEM 4/DAAL/LAB 7/q2.c
@@ -2,19 +2,6 @@
 #include <stdlib.h>
 #include "BST.c"
 
-int max(int a,int b)
-{
-    return a>b?a:b;
-}
-
-int height(Nodeptr root)
-{
-    if(root)
-    {
-        return max(root->lchild,root->rchild)+1;
-    }
-}
-
 int balfac(Nodeptr root)
 {
     return height(root->lchild)-height(root->rchild);
@@ -51,33 +38,62 @@ Nodeptr Insert_AVL(Nodeptr root,int item)
         return root;
     }
     if (item<root->data)
-            root->lchild = Insert_BST(root->lchild, item);
+            root->lchild = Insert_AVL(root->lchild, item);
         else if (item>root->data)
-            root->rchild = Insert_BST(root->rchild, item);
+            root->rchild = Insert_AVL(root->rchild, item);
         else
             return root;
     
     int bal=balfac(root);
 
     if (bal > 1 && item < root->lchild->data)
-        return rightRotate(root);
+        return rightrotate(root);
  
     if (bal < -1 && item > root->rchild->data)
-        return leftRotate(root);
+        return leftrotate(root);
 
     if (bal > 1 && item > root->lchild->data)
     {
-        root->lchild =  leftRotate(root->lchild);
-        return rightRotate(root);
+        root->lchild =  leftrotate(root->lchild);
+        return rightrotate(root);
     }
  
     if (bal < -1 && item < root->rchild->data)
     {
-        root->rchild = rightRotate(root->rchild);
-        return leftRotate(root);
+        root->rchild = rightrotate(root->rchild);
+        return leftrotate(root);
     }
  
     return root;
+}
+
+Nodeptr create_AVL(Nodeptr root,int data)
+{
+    int x;
+    root=getnode();
+    root->data=data;
+    root->lchild=root->rchild=NULL;
+    while(1)
+    {
+        printf("Enter data to insert (-1 to exit) : \t");
+        scanf("%d",&x);
+        if(x==-1)
+        {
+            break;
+        }
+        root=Insert_AVL(root,x);
+    }
+    return root;
+}
+
+void preorder(Nodeptr root)
+{
+    if(root)
+    {
+        printf("%d ",root->data);
+        preorder(root->lchild);
+        preorder(root->rchild);
+    }
 }
 
 void main()
@@ -86,5 +102,8 @@ void main()
     int data;
     printf("Enter Root Data : \t");
     scanf("%d",&data);
-    root=create_BST(root,data);
+    root=create_AVL(root,data);
+    printf("\n");
+    preorder(root);
+    printf("\n");
 }
