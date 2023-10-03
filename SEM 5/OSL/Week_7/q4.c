372374b1039d668bec46253eae95b1b933a2710c
the-darklord <saimihirnath@protonmail.com>
2023-10-03 16:19:20 +0530

    OS L7

--- SEM 5/OSL/Week_7/q4.c
+++ SEM 5/OSL/Week_7/q4.c
@@ -1,55 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <semaphore.h>
+#include<semaphore.h>
+#include<pthread.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
 
-sem_t customer, barber;
-pthread_mutex_t seat;
-int free1 = 10;
+sem_t waiting,barber,mutex;
+int f,r;
+int maxSeats[5];
+int i;
 
-void *barb(void *args) {
-	while (1) {
-		sem_wait(&customer);
-		pthread_mutex_lock(&seat);
+void* customer(void* param){
+	while(1){
+		sem_wait(&waiting);
+		printf("Customer %d is waiting\n",i);
+		maxSeats[(++r)%5] = i;
+		i++;
+		sem_post(&mutex);
+		sleep(1);
+	}
+}
 
-		if (free1 < 10)
-			free1++;
-		sleep(2);
-
-		printf("Cutting Completed! Free Seats: %d\n", free1);
+void* barb(void* param){
+	while(1){
+		sem_wait(&barber);
+		sem_wait(&mutex);
+		int item = maxSeats[(++f)%5];
+		sem_post(&waiting);
+		printf("Cutting Started for %d\n",item);
+		sleep(4);
+		printf("Cutting complete for %d\n",item);
 		sem_post(&barber);
-
-		pthread_mutex_unlock(&seat);
 	}
 }
 
-void *cust(void *args) {
-	while (1) {
-		pthread_mutex_lock(&seat);
+int main(){
+	pthread_t cust,bar;
 
-		if (free1 > 0) {
-			free1--;
-			printf("Customer Waiting! Free Seats: %d\n", free1);
-			sem_post(&customer);
-			pthread_mutex_unlock(&seat);
-			sem_wait(&barber);
-		}
-		else
-			pthread_mutex_unlock(&seat);
-	}
-}
+	//Available chairs are 5
+	sem_init(&waiting,0,5);
+	sem_init(&barber,0,1);
+	sem_init(&mutex,0,0);
 
-int main() {
-	pthread_t threads[2];
-	sem_init(&barber, 0, 1);
-	sem_init(&customer, 0, 1);
-	pthread_mutex_init(&seat, 0);
-	pthread_create(&threads[0], NULL, &barb, NULL);
-	pthread_create(&threads[1], NULL, &cust, NULL);
-	pthread_join(threads[0], NULL);
-	pthread_join(threads[1], NULL);
-	sem_destroy(&barber);
-	sem_destroy(&customer);
-	pthread_mutex_destroy(&seat);
+	pthread_create(&cust,0,&customer,NULL);
+	pthread_create(&bar,0,&barb,NULL);
+
+	pthread_join(cust,NULL);
+	pthread_join(bar,NULL);
+
 }
